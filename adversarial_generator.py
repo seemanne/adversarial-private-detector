5e8843bd0104b928e0d5a350185f3a35f8a3a8bc
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2022-11-04 10:46:56 +0100

    add model and dataset

--- adversarial_generator.py
+++ adversarial_generator.py
@@ -2,10 +2,33 @@ import tensorflow as tf
 from private_detector.image_dataset import ImageDataset
 from private_detector.private_detector import PrivateDetector
 
+checkpoint_dir = 'saved_checkpoint'
+
 loss_object = tf.keras.losses.CategoricalCrossentropy(label_smoothing= 0.1, from_logits=True)
 
-model = PrivateDetector(inital_learning_rate = 1e-4, class_labels= [0,1])
-restore_path = tf.train.latest_checkpoint('saved_checkpoint')
+dataset = ImageDataset(
+    classes_files='imagefiles.json',
+    batchsize = 1,
+    steps_per_epoch=1,
+    rotation_augmentation=0,
+    use_augmentation=False,
+    scale_crop_augmentation=1,
+    image_dtype=tf.dtypes.float32
+)
+
+
+model = PrivateDetector(
+    inital_learning_rate = 1e-4,
+    class_labels = dataset.classes,
+    checkpoint_dir=checkpoint_dir,
+    batch_size=1,
+    use_fp16=False,
+    reg_loss_weight=0,
+    tensorboard_log_dir='',
+    eval_threshold=0.5
+)
+
+restore_path = tf.train.latest_checkpoint(checkpoint_dir)
 
 
 
